f8de621c30aea17d0f06b672e6b9d9d89a8c9347
israelonilink <israel_cursos2011@hotmail.com>
2025-10-25 09:57:16 -0600

    24. Comenzando la clase Bee, sus propiedades y métodos principales

--- app/classes/Bee.php
+++ app/classes/Bee.php
@@ -1,1 +1,55 @@
 <?php
+
+class Bee {
+    //Propiedades del framweork
+    private $framework = 'Bee Framework';
+    private $version = '1.0.0';
+    private $uri = [];
+
+    //La función princpal que se ejecuta al instanciar nuestra clase
+    function __construct() {
+
+    }
+
+    /**
+     *Metodo para ejecutar cada "metodo" de forma subsecuente
+     *
+     * @return void
+     */
+
+    private function init(){
+        //Todos los metodos que se quieren ejecutar consecutivamente
+
+    }
+
+    /**
+     *Metodo para iniciar sesión en el sistema
+     *
+     * @return void
+     */
+    private function init_session(){
+
+        if(!session_start()){
+            session_start();
+        }
+        return;
+    }
+
+    /**
+     *Metodo para cargar la configuración del sistema
+     *
+     * @return void
+     */
+
+    private function init_load_config(){
+        $file = 'bee_config.php';
+        if(!is_file('app/config/'.$file)){
+            die(sprintf('El archivo %s no se encuentra, es requerido para que %s funcione.',$file,$this->framework));
+        }
+
+        //Cargando el archivo de configuración
+        require_once("app/config/".$file);
+
+        return;
+    }
+}
--- index.php
+++ index.php
@@ -1,17 +1,1 @@
 <?php
-require_once 'Persona.php';
-
-/*
---EJEMPLO 1
-
-$persona1 = new Persona;
-echo $persona1->crear_persona();
-echo '<br>';
-$persona2 = new Persona;
-echo $persona2->crear_persona();
-*/
-
-echo Persona::crear().'<br>';
-echo Persona::crear().'<br>';
-echo Persona::crear().'<br>';
-echo Persona::crear().'<br>';
