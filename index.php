da2914b5d05625eefa48b8097411b125c5d749a3
israelonilink <israel_cursos2011@hotmail.com>
2025-10-25 08:05:10 -0600

    19. Como es el flujo de lectura del código p1

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo 'Hola mundo estoy funcionando';
+<?php require_once 'app/config/bee_config.php' ?>
+<?php require_once 'app/functions/bee_core_functions.php' ?>
+<?php echo URL; ?>
+<?php echo "<br>"; ?>
+<?php echo en_core() ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+</head>
+<body>
+
+</body>
+</html>
 
